82e2bd9f09467f60d11e17b8427db7af6d7b9965
edson <edson.dev10@gmail.com>
2024-12-22 15:46:22 -0300

    finalizando primeira versao

---
+++ MoneyExtensions.Tests/MoneyExtensionTests.cs
@@ -0,0 +1,14 @@
+﻿namespace MoneyExtensions.Tests;
+
+[TestClass]
+public sealed class MoneyExtensionTests
+{
+    [TestMethod]
+    public void ShouldConvertDecimalToInt()
+    {
+        decimal valor = 279.98M;
+        var cents = valor.ToCents();
+
+        Assert.AreEqual(27998, cents);
+    }
+}
